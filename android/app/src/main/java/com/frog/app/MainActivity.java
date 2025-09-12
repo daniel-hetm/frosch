ab6e3e61f5c7e12d18ed70aa4cfb82861b2b5516
Hettmann <daniel.hettmann@iosb.fraunhofer.de>
2025-09-12 08:47:11 +0200

    rename folder and other things

---
+++ android/app/src/main/java/com/frog/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.frog.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.frog.app',
+  appName: 'frog-rater',
+  webDir: 'dist/AngularTest/browser',
+};
+
+export default config;
