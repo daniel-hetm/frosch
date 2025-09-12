02fe96c3d08a5a57dcc94b30da755070c8bff805
Hettmann <daniel.hettmann@iosb.fraunhofer.de>
2025-09-12 09:22:39 +0200

    cleanup

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.frog.app',
-  appName: 'frog-rater',
-  webDir: 'dist/AngularTest/browser',
-};
-
-export default config;
